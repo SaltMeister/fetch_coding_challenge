13b3a8096e03944ac23c61d9b7f1706cb521a60a
Simon Huang <huisimonliu@gmail.com>
2024-08-02 22:54:50 -0700

    Initial Commit

---
+++ RecipeBrowserApp/RecipeBrowserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowserAppApp.swift
+//  RecipeBrowserApp
+//
+//  Created by Simon Huang on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBrowserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
